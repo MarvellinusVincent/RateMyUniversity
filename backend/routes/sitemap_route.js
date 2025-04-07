ef674ed4ef621b636cbf781d3f011dec9018803c
Marvell <vmarvellinus@gmail.com>
2025-04-07 15:47:37 -0400

    rollback to no sitemap

--- backend/routes/sitemap_route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const sitemapController = require('../controllers/sitemap_controller');
-
-router.get('/sitemap-index.xml', sitemapController.generateSitemapIndex);
-router.get('/sitemap.xml', sitemapController.generateMainSitemap);
-router.get('/sitemap-universities.xml', sitemapController.generateUniversitiesSitemap);
-
-module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,6 @@ const reviewRoutes = require('./routes/review_route');
 const specificUniversityRoutes = require('./routes/university');
 const searchUniversityRoutes = require('./routes/search_university');
 const emailRoutes = require('./routes/email_route');
-const sitemapRoutes = require('./routes/sitemap_route');
 
 app.use(bodyParser.json());
 
@@ -31,7 +30,6 @@ app.use('/reviews', reviewRoutes);
 app.use('/specificUni', specificUniversityRoutes);
 app.use('/searchUniversity', searchUniversityRoutes);
 app.use('/email', emailRoutes);
-app.use('/', sitemapRoutes);
 
 const { pool } = require('./config/db');
 pool.connect()
